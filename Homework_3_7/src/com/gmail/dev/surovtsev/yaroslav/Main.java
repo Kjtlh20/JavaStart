44015093283fe68040e6a1bbb2236d922b6e76f1
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-08-10 22:26:34 +0300

    Create Main.java
    did homework 3.7

---
+++ Homework_3_7/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,21 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input four digit number");
+        int number = sc.nextInt();
+        int digit1 = number / 1000;
+        int digit2 = number / 100 % 10;
+        int digit3 = number / 10 % 10;
+        int digit4 = number % 10;
+        if (digit1 + digit2 == digit3 + digit4) {
+            System.out.println("Lucky ticket");
+        } else {
+            System.out.println("Ticket is not lucky");
+        }
+    }
+}
