3a3216a633778b2d016aedd315958e73762b39a7
Kjtlh20 <yaroslav.surovtsev.dev@gmail.com>
2021-08-10 18:12:04 +0300

    did homework 3.4

---
+++ Homework_3_4/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -0,0 +1,21 @@
+package com.gmail.dev.surovtsev.yaroslav;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input side a");
+        int a = sc.nextInt();
+        System.out.println("Input side b");
+        int b = sc.nextInt();
+        System.out.println("Input side c");
+        int c = sc.nextInt();
+        if (a + b > c && b + c > a && c + a > b) {
+            System.out.println("Triangle exists");
+        } else {
+            System.out.println("Wrong triangle");
+        }
+    }
+}
