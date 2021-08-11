2b20ab6586c2ef29fd01f031c3036264345445db
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-08-11 15:46:31 +0300

    did homework 5.4

--- Homework_5_4/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_5_4/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,8 +1,20 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input text");
+        String text = sc.nextLine();
+        char[] a = text.toCharArray();
+        int c = 0;
+        for (char el : a) {
+            if (el == 'b') {
+                c = c + 1;
+            }
+        }
+        System.out.println("Number of characters 'b': " + c);
     }
 }
