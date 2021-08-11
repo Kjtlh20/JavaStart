e30c9801d13b3827052a2571defeb941e19c69aa
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-08-11 15:38:20 +0300

    did homework 5.3

--- Homework_5_3/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_5_3/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,8 +1,21 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int n = 15;
+        Random random = new Random();
+        int[] a = new int[n];
+        int[] b = new int[n * 2];
+        for (int i = 0; i < n; i++) {
+            a[i] = random.nextInt(11);
+            b[i] = a[i];
+            b[i + n] = a[i] * 2;
+        }
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
     }
 }
