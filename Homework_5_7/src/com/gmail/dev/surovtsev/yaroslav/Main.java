8039952179cebf3ca64c73e8724ed153fc844867
Yaroslav Surovtsev <yaroslav.surovtsev.dev@gmail.com>
2021-08-12 10:32:39 +0300

    did homework 5.7

--- Homework_5_7/src/com/gmail/dev/surovtsev/yaroslav/Main.java
+++ Homework_5_7/src/com/gmail/dev/surovtsev/yaroslav/Main.java
@@ -1,6 +1,5 @@
 package com.gmail.dev.surovtsev.yaroslav;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,63 +8,66 @@ public class Main {
         String[] a = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven"
                 , "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
                 , "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety", "hundred"
-                , "thousand", "million", "billion"};
+                , "thousand", "million"};
         int[] b = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30, 40, 50, 60, 70, 80, 90
-                , 100, 1000, 1000000, 1000000000};
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Input sum");
-//        double sum = sc.nextDouble();
-        //double sum = 444333777555.22;
-        double sum = 525.14;
-        int billions = (int) (sum / 1000000000);
+                , 100, 1000, 1000000};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input sum");
+        double sum = sc.nextDouble();
         int millions = (int) (sum / 1000000 % 1000);
         int thousands = (int) (sum / 1000 % 1000);
-        int hundreds = (int) (sum / 100 % 10);
-        int tens = (int) (sum % 100);
-        int units = (int) (sum % 10);
+        int hundreds = (int) (sum % 1000);
         int cents = (int) (sum * 100 % 100);
-        System.out.println("billions: " + billions);
-        System.out.println("millions: " + millions);
-        System.out.println("thousands: " + thousands);
-        System.out.println("hundreds: " + hundreds);
-        System.out.println("tens: " + tens);
-        System.out.println("units: " + units);
-        System.out.println("cents: " + cents);
-        int index1 = -1;
-        int index2 = -1;
         String text = "";
-        if (hundreds != 0) {
-            index1 = getArrayIndex(b, hundreds);
-            index2 = getArrayIndex(b, 100);
-            text = text + a[index1] + " " + a[index2] + " ";
+        String textMillions = getNumberInWords(millions, 1000000, a, b);
+        if (!textMillions.isEmpty()) {
+            text = text + textMillions;
         }
-        if (tens != 0) {
-            if (tens < 20) {
-                index1 = getArrayIndex(b, tens);
-                text = text + a[index1];
-            } else {
-                int tens1 = tens / 10 * 10;
-                int tens2 = tens % 10;
-                index1 = getArrayIndex(b, tens1);
-                index2 = getArrayIndex(b, tens2);
-                text = text + a[index1] + " " + a[index2] + " ";
-            }
+        String textThousands = getNumberInWords(thousands, 1000, a, b);
+        if (!textThousands.isEmpty()) {
+            text = text + textThousands;
         }
-        text = text + "dollars ";
-        if (cents != 0) {
-            if (cents < 20) {
-                index1 = getArrayIndex(b, cents);
-                text = text + a[index1] + " ";
-            } else {
-                int cents1 = cents / 10 * 10;
-                int cents2 = cents % 10;
-                index1 = getArrayIndex(b, cents1);
-                index2 = getArrayIndex(b, cents2);
-                text = text + a[index1] + " " + a[index2] + " ";
-            }
+        String textHundreds = getNumberInWords(hundreds, 0, a, b);
+        if (!textHundreds.isEmpty()) {
+            text = text + textHundreds;
         }
-        text = text + "cents ";
+        if (!text.isEmpty()) {
+            text = text + "dollars ";
+        }
+        String textCents = getNumberInWords(cents, 0, a, b);
+        if (!textCents.isEmpty()) {
+            text = text + textCents + "cents";
+        }
         System.out.println(text);
+    }
+
+    public static String getNumberInWords(int number, int rank, String[] a, int[] b) {
+        String text = "";
+        int index;
+        int number100 = number / 100;
+        int number10 = number % 100 / 10;
+        int number1 = number % 10;
+        if (number100 != 0) {
+            index = getArrayIndex(b, number100);
+            text = text + a[index] + " hundreds ";
+        }
+        if (number10 != 0) {
+            if (number10 * 10 + number1 <= 20) {
+                index = getArrayIndex(b, number10 * 10 + number1);
+            } else {
+                index = getArrayIndex(b, number10 * 10);
+            }
+            text = text + a[index] + " ";
+        }
+        if (number1 != 0 && (number10 * 10 + number1 > 20)) {
+            index = getArrayIndex(b, number1);
+            text = text + a[index] + " ";
+        }
+        index = getArrayIndex(b, rank);
+        if (number != 0 && index != -1) {
+            text = text + a[index] + " ";
+        }
+        return text;
     }
 
     public static int getArrayIndex(int[] a, int value) {
